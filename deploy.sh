4e74dee2be55932e9e353e5e76ebe236a0fbc49e
Thomas Reece <tomreece@gmail.com>
2024-04-02 08:42:51 -0500

    Add DS_Store to exclude

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd public
-aws s3 cp . s3://www.38square.com/ --recursive
+aws s3 cp . s3://www.38square.com/ --recursive --exclude ".DS_Store"
 aws cloudfront create-invalidation --distribution-id E3R99OCEXVBV30 --paths "/*"
